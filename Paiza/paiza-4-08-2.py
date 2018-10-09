28d16b9bd27fa7bb0b826bcb1b6abc48410ce7c1
ytamu1116 <43875367+ytamu1116@users.noreply.github.com>
2018-10-09 23:13:37 +0900

    1011課題　田村　祐毅

--- Paiza/paiza-4-08-2.py
+++ Paiza/paiza-4-08-2.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+﻿# coding: utf-8
 
 '''
 演習課題「おみくじを作ろう」
@@ -26,6 +26,14 @@ line = input().rstrip()
 # 今回は自力で全部書いてみよう！
 
 # カンマで分割して、リストに代入
+omikuji = line.split(",")
+
 # リストの要素数を変数に代入
+
+num = len(omikuji)
+
 # リストの中身を出力
-# ランダムに選んだリストの要素を出力
+print(omikuji)
+
+# ランダムに選んだリストの要
+print(omikuji[random.randrange(num)])
